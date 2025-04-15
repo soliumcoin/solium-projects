3847cba668ac02cf73438b08600830316bdacbb4
soliumcoin <Vedattunc1453@gmail.com>
2025-04-15 12:17:14 +0300

    Update soliumcoin.sol

--- soliumcoin.sol
+++ soliumcoin.sol
@@ -1,1 +1,60 @@
+/**
+ *Submitted for verification at BscScan.com on 2025-04-14
+*/
 
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.26;
+
+contract soliumcoin {
+    string public name = "SOLIUM";
+    string public symbol = "SLM";
+    uint8 public decimals = 18;
+    uint256 public totalSupply;
+
+    address public owner;
+
+    mapping(address => uint256) public balanceOf;
+    mapping(address => mapping(address => uint256)) public allowance;
+
+    event Transfer(address indexed from, address indexed to, uint256 value);
+    event Approval(address indexed owner, address indexed spender, uint256 value);
+
+    modifier onlyOwner() {
+        require(msg.sender == owner, "You are not the owner");
+        _;
+    }
+
+    constructor() {
+        owner = msg.sender;
+        totalSupply = 100000000 * (10 ** uint256(decimals)); // 100 mn solium coin
+        balanceOf[owner] = totalSupply;
+        emit Transfer(address(0), owner, totalSupply);
+    }
+
+    function transfer(address to, uint256 amount) external returns (bool) {
+        require(balanceOf[msg.sender] >= amount, "Not enough balance");
+        _transfer(msg.sender, to, amount);
+        return true;
+    }
+
+    function _transfer(address from, address to, uint256 amount) internal {
+        require(to != address(0), "Invalid address");
+        balanceOf[from] -= amount;
+        balanceOf[to] += amount;
+        emit Transfer(from, to, amount);
+    }
+
+    function approve(address spender, uint256 amount) external returns (bool) {
+        allowance[msg.sender][spender] = amount;
+        emit Approval(msg.sender, spender, amount);
+        return true;
+    }
+
+    function transferFrom(address from, address to, uint256 amount) external returns (bool) {
+        require(balanceOf[from] >= amount, "Not enough balance");
+        require(allowance[from][msg.sender] >= amount, "Not approved");
+        allowance[from][msg.sender] -= amount;
+        _transfer(from, to, amount);
+        return true;
+    }
+}
